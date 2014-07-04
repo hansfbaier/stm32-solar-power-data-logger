4be6450cfdcbb0a8ca814ed55aad6ea43b4c4648
Hans Baier <hansfbaier@googlemail.com>
2014-07-04 22:03:54 +0700

    make it blink (tm)

---
+++ flash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+openocd -f openocd.cfg -c init -c "reset init" -c "halt" &
+sleep 8
+nc localhost 4444 <<EOF
+flash write_image erase main.bin 0x8000000 bin verify
+reset run
+EOF
+pkill openocd
+echo Done
