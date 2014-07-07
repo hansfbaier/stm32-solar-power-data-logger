79b6be0fbae619005ddf2638c8d55d4c49b2d352
Hans Baier <hansfbaier@googlemail.com>
2014-07-07 16:35:38 +0700

    adapt to STM32F103VCT6 - Board

--- main.c
+++ main.c
@@ -20,36 +20,12 @@ int main(void) {
     GPIO_Config();
 
     for(;;) {
-        GPIO_SetBits(GPIOA, GPIO_Pin_0);
-        GPIO_ResetBits(GPIOA, GPIO_Pin_7);
+        GPIO_SetBits(GPIOB, GPIO_Pin_0);
+        GPIO_ResetBits(GPIOB, GPIO_Pin_1);
         Delay(0xAFFFF);
 
-        GPIO_ResetBits(GPIOA, GPIO_Pin_0);
-        GPIO_SetBits(GPIOA, GPIO_Pin_1);
-        Delay(0xAFFFF);
-
-        GPIO_ResetBits(GPIOA, GPIO_Pin_1);
-        GPIO_SetBits(GPIOA, GPIO_Pin_2);
-        Delay(0xAFFFF);
-
-        GPIO_ResetBits(GPIOA, GPIO_Pin_2);
-        GPIO_SetBits(GPIOA, GPIO_Pin_3);
-        Delay(0xAFFFF);
-
-        GPIO_ResetBits(GPIOA, GPIO_Pin_3);
-        GPIO_SetBits(GPIOA, GPIO_Pin_4);
-        Delay(0xAFFFF);
-
-        GPIO_ResetBits(GPIOA, GPIO_Pin_4);
-        GPIO_SetBits(GPIOA, GPIO_Pin_5);
-        Delay(0xAFFFF);
-
-        GPIO_ResetBits(GPIOA, GPIO_Pin_5);
-        GPIO_SetBits(GPIOA, GPIO_Pin_6);
-        Delay(0xAFFFF);
-
-        GPIO_ResetBits(GPIOA, GPIO_Pin_6);
-        GPIO_SetBits(GPIOA, GPIO_Pin_7);
+        GPIO_ResetBits(GPIOB, GPIO_Pin_0);
+        GPIO_SetBits(GPIOB, GPIO_Pin_1);
         Delay(0xAFFFF);
     }
 }
@@ -62,10 +38,10 @@ void Delay(volatile unsigned long delay) {
 void GPIO_Config() {
     GPIO_InitTypeDef	GPIO_InitStructure;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
