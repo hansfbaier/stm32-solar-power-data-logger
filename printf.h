4b3b8592795ebf48d06e8fb19b0189b75a99422a
Hans Baier <hansfbaier@gmail.com>
2017-05-17 18:44:36 +0700

    whitespace

--- printf.h
+++ printf.h
@@ -111,7 +111,7 @@ regs Kusti, 23.10.2004
 void init_printf(void* putp,void (*putf) (void*,char));
 
 void tfp_printf(char *fmt, ...);
-void tfp_sprintf(char* s,char *fmt, ...);
+void tfp_sprintf(char* s, char *fmt, ...);
 
 void tfp_format(void* putp,void (*putf) (void*,char),char *fmt, va_list va);
 
